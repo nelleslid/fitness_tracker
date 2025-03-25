95f2cd803f9614fff82dbaac674fa386486a891e
Nelles Lid <nelleslid@MacBookPro.fritz.box>
2025-03-25 22:31:27 +0100

    auth and home_page extraction

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class DashboardPage extends StatelessWidget {
-  const DashboardPage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
